ff9419b5d5848258493554d6873e2da328b00cc0
Vitaly <iwitaly@me.com>
2014-03-08 23:13:00 +0400

    skin recogniser

--- wordAnalizer/main.py
+++ wordAnalizer/main.py
@@ -1,26 +1,14 @@
 from os import walk
-from sys import argv
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn import cross_validation
 import sklearn
 import numpy as np
+import difflib
 
-def readTexts(dirpath):
-	textList = []
-	for root, dirs, files in walk(dirpath):
-	    for file in files:
-			fin = open(dirpath + '/' + file, 'r')
-			text = ""
-			for line in fin.readlines():
-				text += unicode(line, errors='replace')
-			textList.append(text)
-			fin.close()
-	return textList
-
-def readFile():
+def readFile(fileName):
     dict = {}
-    with open('italian.txt.learn') as f:
+    with open(fileName) as f:
         rows = f.readlines()
         for row in rows:
             components = row.strip().split('\t')
@@ -37,14 +25,11 @@ def readFile():
 
 def difference(dict):
 
-    print dict.items()[0]
-
     for key in dict.keys():
         i = 0
         flag = True
 
-        while flag and len(dict[key][0][0]) > i:
-
+        while (flag and len(dict[key][0][0]) > i):
             letter = dict[key][0][0][i]
             for word in dict[key]:
                 if len(word[0]) == i or letter != word[0][i]:
@@ -54,7 +39,19 @@ def difference(dict):
 
         print key, key[i:], i
 
-readFile()
-difference(readFile())
+def newDifference(dict):
+    yTrain = []
+    for key in dict.keys():
+        val = dict[key]
+        x = [difflib.SequenceMatcher(a=key, b=word[0]).get_matching_blocks()[0] for word in val]
+        #get suffixes
+        for i in xrange(len(x)):
+            howManyCharactersAreEqual = x[i][2]
+            maxString = val[i][0]
+            tupleToAppend = (maxString[howManyCharactersAreEqual:], len(maxString)-howManyCharactersAreEqual)
+            yTrain.append(tupleToAppend)
+    print yTrain
 
-vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(3,5))
+newDifference(readFile('italian.txt.learn'))
+
+#vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(3,5))
